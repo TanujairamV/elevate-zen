18c4b07d9d7406f828d584416746460cd3a39e13
TanujairamV <88611197+TanujairamV@users.noreply.github.com>
2026-08-26 16:02:19 +0530

    Changed app name and id

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.techtonic.elevate_zen"
+    namespace = "com.techtonic.elevatezen"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
